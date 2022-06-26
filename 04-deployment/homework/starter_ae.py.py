2380f9c7b415e4741e7f84bf7637e93dc33fc081
Ubuntu <ubuntu@ip-172-31-0-105.eu-central-1.compute.internal>
2022-06-26 16:57:38 +0000

    add script for homework week4 Q5

--- 04-deployment/homework/starter_ae.py.py
+++ 04-deployment/homework/starter_ae.py.py
@@ -1,59 +1,67 @@
-# %%
-!pip freeze | grep scikit-learn
-
-# %%
 import pickle
 import pandas as pd
+import numpy as np
+import argparse
 
-# %%
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-# %%
-categorical = ['PUlocationID', 'DOlocationID']
+def load_model():
+    with open('model.bin', 'rb') as f_in:
+        (dv, model) = pickle.load(f_in)
+        return  (dv, model)
 
 def read_data(filename):
     df = pd.read_parquet(filename)
+    print("file read")
+    
     
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
-
+    
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
+    print("features added")
+
     return df
 
-# %%
-df = read_data('https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-02.parquet')
+def predict(year, month):
+    print(f"data file is https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{year:04d}-{month:02d}")
 
-# %%
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+    df = read_data(f'https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{year:04d}-{month:02d}.parquet')
 
-# %%
-import numpy as np
-np.mean(y_pred)
+    (dv, model) = load_model()
+    print("model loaded")
 
-# %%
-df.head()
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
 
-# %%
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    print("final df to parquet")
+    df_result.to_parquet(
+        f'df_results{year:04d}-{month:02d}.parquet',
+        engine='pyarrow',
+        compression=None,
+        index=False
+    )
+    return y_pred
 
-df['ride_id'] = f'{2021:04d}/{2:02d}_' + df.index.astype('str')
-df['ride_id']
+if __name__ == '__main__':
 
-# %%
-df_result = pd.DataFrame()
-df_result['ride_id'] = df[['ride_id']]
-df_result['pred'] = y_pred
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--year",
+        default= 2021
+    )
+    parser.add_argument(
+        "--month",
+        default= 2
+    )
 
-df_result.to_parquet(
-    'df_results.parquet',
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
+    args = parser.parse_args()
+    categorical = ['PUlocationID', 'DOlocationID']
+
+
+    print(np.mean(predict(int(args.year), int(args.month))))
 
 
